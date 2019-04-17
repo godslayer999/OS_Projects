1ee8471af675d039367c6472cf6e6974b7170b90
godslayer999 <36673450+godslayer999@users.noreply.github.com>
2019-04-17 11:45:10 +0530

    Added readability
    replaced old variable names with sensible variable names and increasing modularity.

--- que24v2.c
+++ que24v2.c
@@ -15,36 +15,22 @@ Consider: 1. Queue 2 will be processed after Queue 1 becomes empty.
 #include <stdio.h>
 #include <string.h>
 
-void main(){
+	char p[10][5],temp[5];
+    	int i,j,b_time[10],w_time[10],totw_time=0,prior[10],temp1,n;
+    	float avgw_time;
 
-    	char p[10][5],temp[5];
-    	int i,j,pt[10],wt[10],totwt=0,pr[10],temp1,n;
-    	float avgwt;
-
-    	printf("enter no of processes:");
-    	scanf("%d",&n);
-
-    	for(i=0;i<n;i++){
-
-        	printf("Enter process%d name:",i+1);
-        	scanf("%s",&p[i]);
-        	printf("Enter Burst time:");
-        	scanf("%d",&pt[i]);
-        	printf("Enter Priority:");
-        	scanf("%d",&pr[i]);
-
-    	}
-	//Using Selection sort to arrange processes based on their priority
-    	for(i=0;i<n-1;i++) {
+//Impementation Selection sort to sort the processes based on their Priority.
+void sort_prior(){
+	for(i=0;i<n-1;i++) {
         	for(j=i+1;j<n;j++) {
-            		if(pr[i]>pr[j]) {
-                		temp1=pr[i];
-                		pr[i]=pr[j];
-                		pr[j]=temp1;
+            		if(prior[i]>prior[j]) {
+                		temp1=prior[i];
+                		prior[i]=prior[j];
+                		prior[j]=temp1;
 
-				temp1=pt[i];
-                		pt[i]=pt[j];
-                		pt[j]=temp1;
+				temp1=b_time[i];
+                		b_time[i]=b_time[j];
+                		b_time[j]=temp1;
 
                 		strcpy(temp,p[i]);
                 		strcpy(p[i],p[j]);
@@ -52,64 +38,84 @@ void main(){
             		}
         	}
     	}
+}
 
-    	wt[0]=0;
+void main(){
+
+    	printf("Enter no of processes:");
+    	scanf("%d",&n);
+
+    	for(i=0;i<n;i++){
+
+        	printf("\nEnter process id: :");
+        	scanf("%s",&p[i]);
+        	printf("Enter Burst time:");
+        	scanf("%d",&b_time[i]);
+        	printf("Enter Priority:");
+        	scanf("%d",&prior[i]);
+
+    	}
+	//Using Selection sort to arrange processes based on their priority
+    	sort_prior();
+
+    	w_time[0]=0;
     	for(i=1;i<n;i++)
     	{
-        	wt[i]=wt[i-1]+wt[i-1];
-        	totwt=totwt+wt[i];
+        	w_time[i]=w_time[i-1]+b_time[i-1];
+        	totw_time=totw_time+w_time[i];
     	}
 
-    	avgwt=(float)totwt/n;
+    	avgw_time=(float)totw_time/n;
 
     	printf("p_name\t p_time\t priority\t w_time\n");
 
 	for(i=0;i<n;i++) {
 
-       		printf(" %s\t %d\t %d\t %d\n" ,p[i],pt[i],pr[i],wt[i]);
+       		printf(" %s\t %d\t %d\t %d\n" ,p[i],b_time[i],prior[i],w_time[i]);
 
     	}
-   	printf("total waiting time=%d\n avg waiting time=%f",totwt,avgwt);
+   	printf("Total Waiting time = %d\nAvg Waiting time = %f\n\n",totw_time,avgw_time);
 
-    	int ts,pid[10],need[10],wt1[10],tat[10],i1,j1,n2,n1;
-    	int bt[10],flag[10],ttat=0,twt=0;
-    	float awt,atat;
+    	int ts,pid[10],r_time[10],w_time1[10],tat[10],i1,j1,n2,n1;
+    	int bt[10],flag[10],ttat=0,tw_time=0;
+    	float aw_time,atat;
 
-    	printf("\nEnter the number of Processes \n");
+    	printf("\nEnter the number of Processes : ");
     	scanf("%d",&n);
     	n1=n;
-    	printf("\n Enter the Timeslice \n");
+    	printf("\n Enter the Timeslice : ");
     	scanf("%d",&ts);
 
     	for(i=1;i<=n;i++ ){
 
-        	printf("\n Enter the process ID %d",i);
+        	printf("\n Enter the process ID : ");
         	scanf("%d",&pid[i]);
-        	printf("\n Enter the Burst Time for the process");
+        	printf("\n Enter the Burst Time for the process: ");
         	scanf("%d",&bt[i]);
-        	need[i]=bt[i];
+        	r_time[i]=bt[i];
     	}
 
     	for(i=1;i<=n;i++){
 
         	flag[i]=1;
-        	wt[i]=0;
+        	w_time[i]=0;
 
     	}
 
+	//Algo for ROUND-ROBIN Scheduling Queue
     	while(n!=0){
 
         for(i=1;i<=n;i++){
 
-            	if(need[i]>=ts){
+            	if(r_time[i]>=ts){
 
                 	for(j=1;j<=n;j++){
 
-                    		if((i!=j)&&(flag[i]==1)&&(need[j]!=0))
-                   	 		wt[j]+=ts;
+                    		if((i!=j)&&(flag[i]==1)&&(r_time[j]!=0))
+                   	 		w_time[j]+=ts;
                 	}
-                	need[i]-=ts;
-                	if(need[i]==0){
+                	r_time[i]-=ts;
+                	if(r_time[i]==0){
 
                     		flag[i]=0;
                     		n--;
@@ -119,10 +125,10 @@ void main(){
 
                 	for(j=1;j<=n;j++){
 
-                    		if((i!=j)&&(flag[i]==1)&&(need[j]!=0))
-                    			wt[j]+=need[i];
+                    		if((i!=j)&&(flag[i]==1)&&(r_time[j]!=0))
+                    			w_time[j]+=r_time[i];
                 	}
-                	need[i]=0;
+                	r_time[i]=0;
                 	n--;
                 	flag[i]=0;
             		}
@@ -132,24 +138,24 @@ void main(){
     	for(i=1;i<=n1;i++)
     	{
 
-        	tat[i]=wt[i]+bt[i];
-        	twt=twt+wt[i];
+        	tat[i]=w_time[i]+bt[i];
+        	tw_time=tw_time+w_time[i];
         	ttat=ttat+tat[i];
 
     	}
 
-    	awt=(float)twt/n1;
+    	aw_time=(float)tw_time/n1;
     	atat=(float)ttat/n1;
 
     	printf("\n\n Process \t Process ID  \t BurstTime \t Waiting Time \t TurnaroundTime \n ");
 
 	for(i=1;i<=n1;i++){
 
-        	printf("\n %5d \t %5d \t\t %5d \t\t %5d \t\t %5d \n", i,pid[i],bt[i],wt[i],tat[i]);
+        	printf("\n %5d \t\t %5d \t\t %5d \t\t %5d \t\t %5d \n", i,pid[i],bt[i],w_time[i],tat[i]);
 
     	}
 
-	printf("\n The average Waiting Time=%4.2f",awt);
-    	printf("\n The average Turn around Time=%4.2f",atat);
+	printf("\n The average Waiting Time=%4.2f",aw_time);
+    	printf("\n The average Turn around Time=%4.2f\n",atat);
 
 }
